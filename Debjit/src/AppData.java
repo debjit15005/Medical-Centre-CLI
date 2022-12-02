c2cc6e0eb380007ba48c84e998d6407b6d75c280
Debjit Kar <you@example.com>
2022-12-02 16:13:58 +0530

    combined code

--- Debjit/src/AppData.java
+++ Debjit/src/AppData.java
@@ -5,4 +5,8 @@ public class AppData {
 	static Map <Integer,Doctor> docs = new HashMap<Integer,Doctor>();
 	//HashMap <Student, Integer> dues = new HashMap<>();
 	static Map<Integer,Student> studs = new HashMap<Integer,Student>();
+	static Map <Integer,Medicine> inventory = new HashMap<Integer,Medicine>();
+	static ArrayList <Notice> notices = new ArrayList<>();
+	static HashMap <Student, Integer> dues = new HashMap<Student,Integer>();
+	static int sale;
 }
